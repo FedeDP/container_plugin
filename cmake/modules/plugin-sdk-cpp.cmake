b6d9c3364c34df1ab8acab36866eb608f908a883
Federico Di Pierro <nierro92@gmail.com>
2024-11-28 12:46:47 +0100

    fix(build): fixed build using njson provided by plugin-sdk-cpp.
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- cmake/modules/plugin-sdk-cpp.cmake
+++ cmake/modules/plugin-sdk-cpp.cmake
@@ -12,4 +12,6 @@ FetchContent_Declare(
 
 FetchContent_MakeAvailable(plugin-sdk-cpp)
 set(PLUGIN_SDK_INCLUDE "${plugin-sdk-cpp_SOURCE_DIR}/include")
+# Since we use nlohmann-json provided by the sdk-cpp plugin, we need to also use use this one
+set(PLUGIN_SDK_DEPS_INCLUDE "${plugin-sdk-cpp_SOURCE_DIR}/include/falcosecurity/internal/deps")
 message(STATUS "Using plugin-sdk-cpp include at '${PLUGIN_SDK_INCLUDE}'")
