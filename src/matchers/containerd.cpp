bd98e597509b127e349a385f6047a68768822538
Federico Di Pierro <nierro92@gmail.com>
2025-01-09 12:53:17 +0100

    new(src/matchers): match containerd cgroup with any namespace.
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- src/matchers/containerd.cpp
+++ src/matchers/containerd.cpp
@@ -1,14 +1,26 @@
 #include "containerd.h"
 #include "runc.h"
+#include <iostream>
+#include <sstream> // for ostringstream
+#include <re2/re2.h>
 
 using namespace libsinsp::runc;
 
-// Containers created via ctr
-// use the "default" namespace (instead of the cri "k8s.io" namespace)
-// which will result in the `/default` cgroup path.
-// https://github.com/containerd/containerd/blob/3b15606e196e450cf817fa9f835ab5324b35a28b/pkg/namespaces/context.go#L32
-constexpr const cgroup_layout CONTAINERD_CGROUP_LAYOUT[] = {{"/default/", ""}, {nullptr, nullptr}};
+static re2::RE2 pattern("^/([A-Za-z0-9]+(?:[._-](?:[A-Za-z0-9]+))*)/$", re2::RE2::POSIX);
 
 bool containerd::resolve(const std::string& cgroup, std::string& container_id) {
-    return matches_runc_cgroup(cgroup, CONTAINERD_CGROUP_LAYOUT, container_id);
+	std::string containerd_namespace;
+	// Containers created via ctr
+	// use a cgroup path like: `0::/namespace/container_id`
+	// Since we cannot know the namespace in advance, we try to
+	// extract it from the cgroup path by following provided regex,
+	// and use that to eventually extract the container id.
+	if(re2::RE2::PartialMatch(cgroup, pattern, &containerd_namespace)) {
+		std::ostringstream out;
+		out << "/" << containerd_namespace << "/";
+		auto layout_str = out.str();
+		cgroup_layout CONTAINERD_CGROUP_LAYOUT[] = {{layout_str.c_str(), ""}, {nullptr, nullptr}};
+		return matches_runc_cgroup(cgroup, CONTAINERD_CGROUP_LAYOUT, container_id);
+	}
+	return false;
 }
