f0473ebdbe77b18de18723204cab2839401085fe
Federico Di Pierro <nierro92@gmail.com>
2025-03-13 10:51:08 +0100

    new(cmake,ci): fetch libbtrfs library with cmake.
    Properly use its headers (we don't need to build the library)
    as CGO_CFLAGS `-I` to allow go-worker to build without the dep on system library.
    
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- cmake/modules/go-worker.cmake
+++ cmake/modules/go-worker.cmake
@@ -6,6 +6,9 @@ include(arch)
 message(STATUS "Building go-worker static library")
 
 if(CMAKE_HOST_SYSTEM_NAME STREQUAL "Linux")
+    # btrfs cmake dep
+    include(btrfs)
+
     ## gpgme is vcpkg installed but comes with pkg-config
     ## It is a runtime dep of podman go package.
     find_package(PkgConfig REQUIRED)
@@ -16,12 +19,13 @@ if(CMAKE_HOST_SYSTEM_NAME STREQUAL "Linux")
     set(VCPKG_PKGCONFIG_PATH "${CMAKE_SOURCE_DIR}/vcpkg/packages/gpgme_${VCPKG_ARCH}-linux/lib/pkgconfig/:${CMAKE_SOURCE_DIR}/vcpkg/packages/libgpg-error_${VCPKG_ARCH}-linux/lib/pkgconfig/:${CMAKE_SOURCE_DIR}/vcpkg/packages/libassuan_${VCPKG_ARCH}-linux/lib/pkgconfig/")
     set(WORKER_DEP PkgConfig::GPGME)
 endif()
+
 ExternalProject_Add(go-worker
         DEPENDS ${WORKER_DEP}
         SOURCE_DIR ${CMAKE_SOURCE_DIR}/go-worker
         BUILD_IN_SOURCE 1
         CONFIGURE_COMMAND ""
-        BUILD_COMMAND make -e PKG_CONFIG_PATH=${VCPKG_PKGCONFIG_PATH} lib
+        BUILD_COMMAND make -e PKG_CONFIG_PATH=${VCPKG_PKGCONFIG_PATH} ${BTRFS_CGO_CFLAG} lib
         BUILD_BYPRODUCTS libworker.a libworker.h
         INSTALL_COMMAND ""
 )
