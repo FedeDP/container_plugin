e72b4a238f15c13cc01a08bcbdd266c8ecd120ac
Federico Di Pierro <nierro92@gmail.com>
2025-03-13 14:59:53 +0100

    fix(cmake): build static on windows and osx too.
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- cmake/modules/vcpkg.cmake
+++ cmake/modules/vcpkg.cmake
@@ -1,6 +1,10 @@
-# Only build release versions of the ports:
-# https://learn.microsoft.com/en-us/vcpkg/users/triplets#vcpkg_build_type
-set(VCPKG_BUILD_TYPE release)
-set(VCPKG_CRT_LINKAGE static)
-set(VCPKG_LIBRARY_LINKAGE static)
+if(CMAKE_HOST_SYSTEM_NAME STREQUAL "Linux")
+    set(VCPKG_TARGET_TRIPLET "${VCPKG_ARCH}-linux-release")
+elseif(CMAKE_HOST_SYSTEM_NAME STREQUAL "Windows")
+    set(VCPKG_TARGET_TRIPLET "${VCPKG_ARCH}-windows-static-md-release")
+elseif(CMAKE_HOST_SYSTEM_NAME STREQUAL "Darwin")
+    set(VCPKG_TARGET_TRIPLET "${VCPKG_ARCH}-osx-release")
+else()
+    message(FATAL_ERROR "Unsupported system: ${CMAKE_HOST_SYSTEM_NAME}")
+endif()
 set(CMAKE_TOOLCHAIN_FILE ${CMAKE_CURRENT_SOURCE_DIR}/vcpkg/scripts/buildsystems/vcpkg.cmake CACHE STRING "Vcpkg toolchain file")
