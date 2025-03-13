a21cbf834e1e3ec6588417ab4d3d9a113fe9722f
Federico Di Pierro <nierro92@gmail.com>
2025-03-13 12:40:35 +0100

    fix(src/matchers): fixed memory bug in containerd matcher.
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- src/matchers/containerd.cpp
+++ src/matchers/containerd.cpp
@@ -7,10 +7,12 @@
 using namespace libsinsp::runc;
 
 static re2::RE2 pattern("/([A-Za-z0-9]+(?:[._-](?:[A-Za-z0-9]+))*)/");
+static cgroup_layout CONTAINERD_CGROUP_LAYOUT[] = {{"SET BY RESOLVE()", ""},
+                                                   {nullptr, nullptr}};
+static std::string containerd_namespace;
 
 bool containerd::resolve(const std::string& cgroup, std::string& container_id)
 {
-    std::string containerd_namespace;
     // Containers created via ctr
     // use a cgroup path like: `0::/namespace/container_id`
     // Since we cannot know the namespace in advance, we try to
@@ -18,11 +20,8 @@ bool containerd::resolve(const std::string& cgroup, std::string& container_id)
     // and use that to eventually extract the container id.
     if(re2::RE2::PartialMatch(cgroup, pattern, &containerd_namespace))
     {
-        std::ostringstream out;
-        out << "/" << containerd_namespace << "/";
-        auto layout_str = out.str();
-        cgroup_layout CONTAINERD_CGROUP_LAYOUT[] = {{layout_str.c_str(), ""},
-                                                    {nullptr, nullptr}};
+        containerd_namespace = "/" + containerd_namespace + "/";
+        CONTAINERD_CGROUP_LAYOUT[0].prefix = containerd_namespace.c_str();
         return matches_runc_cgroup(cgroup, CONTAINERD_CGROUP_LAYOUT,
                                    container_id, true);
     }
