13f4371063ee68cc78257367014ceecf212ff6aa
Federico Di Pierro <nierro92@gmail.com>
2025-03-21 11:48:07 +0100

    cleanup(build): disable gpgme dep.
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- cmake/modules/go-worker.cmake
+++ cmake/modules/go-worker.cmake
@@ -5,20 +5,9 @@ message(STATUS "Building go-worker static library")
 if(CMAKE_HOST_SYSTEM_NAME STREQUAL "Linux")
     # btrfs cmake dep
     include(btrfs)
-
-    ## gpgme is vcpkg installed but comes with pkg-config
-    ## It is a runtime dep of podman go package.
-    find_package(PkgConfig REQUIRED)
-    pkg_check_modules(GPGME REQUIRED gpgme IMPORTED_TARGET)
-
-    # Pkg config paths to all gpgme libs (libgpgme, libgpg-error and libassuan)
-    # to be passed down to go-worker to let it find the needed deps.
-    set(VCPKG_PKGCONFIG_PATH "${CMAKE_SOURCE_DIR}/vcpkg/packages/gpgme_${VCPKG_ARCH}-linux-release/lib/pkgconfig/:${CMAKE_SOURCE_DIR}/vcpkg/packages/libgpg-error_${VCPKG_ARCH}-linux-release/lib/pkgconfig/:${CMAKE_SOURCE_DIR}/vcpkg/packages/libassuan_${VCPKG_ARCH}-linux-release/lib/pkgconfig/")
-    set(WORKER_DEP PkgConfig::GPGME)
 endif()
 
 ExternalProject_Add(go-worker
-        DEPENDS ${WORKER_DEP}
         SOURCE_DIR ${CMAKE_SOURCE_DIR}/go-worker
         BUILD_IN_SOURCE 1
         CONFIGURE_COMMAND ""
