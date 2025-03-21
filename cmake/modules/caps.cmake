63ecbd039c739a40b759231e651589a2d6182943
Federico Di Pierro <nierro92@gmail.com>
2025-03-21 11:48:07 +0100

    cleanup(build): disable gpgme dep.
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- cmake/modules/caps.cmake
+++ cmake/modules/caps.cmake
@@ -8,9 +8,6 @@ macro(ADD_CAP cap)
         string(TOLOWER ${cap} lower_cap)
         file(GLOB_RECURSE SOURCES src/caps/${lower_cap}/*.cpp)
         list(APPEND CAPS_SOURCES ${SOURCES})
-
-        # Append to vcpkg manifest features - only useful for ASYNC cap
-        list(APPEND VCPKG_MANIFEST_FEATURES ${lower_cap})
     endif()
 endmacro()
 
